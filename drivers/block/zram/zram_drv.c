db85d3cd22dc1ac337a9b9acf4efcf0676da68e6
Marcelo Reis <marcelo-fil@outlook.com>
2015-11-28 19:27:57 +0000

    Revert "zram: remove zram->lock in read path and change it with mutex"
    This reverts commit 4a3028ccbe505c68adf239ddded7e06f98e4e93a.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -237,7 +237,6 @@ static struct zram_meta *zram_meta_alloc(u64 disksize)
 	}
 
 	rwlock_init(&meta->tb_lock);
-	mutex_init(&meta->buffer_lock);
 	return meta;
 
 free_table:
@@ -421,7 +420,6 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec, u32 index,
 	unsigned char *user_mem, *cmem, *src, *uncmem = NULL;
 	struct zram_meta *meta = zram->meta;
 	static unsigned long zram_rs_time;
-	bool locked = false;
 
 	page = bvec->bv_page;
 	src = meta->compress_buffer;
@@ -442,8 +440,6 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec, u32 index,
 			goto out;
 	}
 
-	mutex_lock(&meta->buffer_lock);
-	locked = true;
 	user_mem = kmap_atomic(page);
 
 	if (is_partial_io(bvec)) {
@@ -470,6 +466,7 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec, u32 index,
 
 	ret = lzo1x_1_compress(uncmem, PAGE_SIZE, src, &clen,
 			       meta->compress_workmem);
+
 	if (!is_partial_io(bvec)) {
 		kunmap_atomic(user_mem);
 		user_mem = NULL;
@@ -528,8 +525,6 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec, u32 index,
 		atomic_inc(&zram->stats.good_compress);
 
 out:
-	if (locked)
-		mutex_unlock(&meta->buffer_lock);
 	if (is_partial_io(bvec))
 		kfree(uncmem);
 
@@ -543,10 +538,15 @@ static int zram_bvec_rw(struct zram *zram, struct bio_vec *bvec, u32 index,
 {
 	int ret;
 
-	if (rw == READ)
+	if (rw == READ) {
+		down_read(&zram->lock);
 		ret = zram_bvec_read(zram, bvec, index, offset, bio);
-	else
+		up_read(&zram->lock);
+	} else {
+		down_write(&zram->lock);
 		ret = zram_bvec_write(zram, bvec, index, offset);
+		up_write(&zram->lock);
+	}
 
 	return ret;
 }
@@ -843,6 +843,7 @@ static int create_device(struct zram *zram, int device_id)
 {
 	int ret = -ENOMEM;
 
+	init_rwsem(&zram->lock);
 	init_rwsem(&zram->init_lock);
 
 	zram->queue = blk_alloc_queue(GFP_KERNEL);
